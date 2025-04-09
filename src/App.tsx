f4781b43a9f12461320d9dfaf83792e05c26b48e
Vaibhav701161 <vaibhavmittal929@gmail.com>
2025-04-09 23:32:36 +0530

    style: global styling tweaks and app entry improvements

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,10 @@ import { Advertisements } from './pages/dashboard/Advertisements';
 import { Influencers } from './pages/dashboard/Influencers';
 import { Profile } from './pages/Profile';
 import { Support } from './pages/Support';
+import { PaymentHistory } from './pages/PaymentHistory';
+import { PaymentTest } from './pages/PaymentTest';
+import { CampaignDetail } from './pages/dashboard/CampaignDetail';
+import { StripeProvider } from './components/payment/StripeProvider';
 
 function App() {
   const { loading } = useAuth();
@@ -23,49 +27,66 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          {/* Public routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/auth/:userType" element={<Auth />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/privacy" element={<Privacy />} />
-          
-          {/* Protected routes that require authentication but not onboarding */}
-          <Route path="/onboarding" element={
-            <ProtectedRoute requiresOnboarding={false}>
-              <Onboarding />
-            </ProtectedRoute>
-          } />
+      <StripeProvider>
+        <Layout>
+          <Routes>
+            {/* Public routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/auth/:userType" element={<Auth />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/privacy" element={<Privacy />} />
+            
+            {/* Protected routes that require authentication but not onboarding */}
+            <Route path="/onboarding" element={
+              <ProtectedRoute requiresOnboarding={false}>
+                <Onboarding />
+              </ProtectedRoute>
+            } />
 
-          {/* Protected routes that require both authentication and completed onboarding */}
-          <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <Overview />
-            </ProtectedRoute>
-          } />
-          <Route path="/advertisements" element={
-            <ProtectedRoute>
-              <Advertisements />
-            </ProtectedRoute>
-          } />
-          <Route path="/influencers" element={
-            <ProtectedRoute>
-              <Influencers />
-            </ProtectedRoute>
-          } />
-          <Route path="/profile" element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          } />
-          <Route path="/support" element={
-            <ProtectedRoute>
-              <Support />
-            </ProtectedRoute>
-          } />
-        </Routes>
-      </Layout>
+            {/* Protected routes that require both authentication and completed onboarding */}
+            <Route path="/dashboard" element={
+              <ProtectedRoute>
+                <Overview />
+              </ProtectedRoute>
+            } />
+            <Route path="/advertisements" element={
+              <ProtectedRoute>
+                <Advertisements />
+              </ProtectedRoute>
+            } />
+            <Route path="/influencers" element={
+              <ProtectedRoute>
+                <Influencers />
+              </ProtectedRoute>
+            } />
+            <Route path="/campaigns/:id" element={
+              <ProtectedRoute>
+                <CampaignDetail />
+              </ProtectedRoute>
+            } />
+            <Route path="/profile" element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            } />
+            <Route path="/support" element={
+              <ProtectedRoute>
+                <Support />
+              </ProtectedRoute>
+            } />
+            <Route path="/payment-history" element={
+              <ProtectedRoute>
+                <PaymentHistory />
+              </ProtectedRoute>
+            } />
+            <Route path="/payment-test" element={
+              <ProtectedRoute>
+                <PaymentTest />
+              </ProtectedRoute>
+            } />
+          </Routes>
+        </Layout>
+      </StripeProvider>
     </BrowserRouter>
   );
 }
